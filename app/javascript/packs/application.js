f0f4d65505d3461db1143de9a62241841d0b87d9
Daniel Muñoz <djmv225@gmail.com>
2020-10-12 10:47:52 -0500

    Some changes for heroku deploy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,35 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+document.addEventListener('turbolinks:load', () => {
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+        const target = el.dataset.target
+        const $target = document.getElementById(target)
+
+        el.classList.toggle('is-active')
+        $target.classList.toggle('is-active')
+      })
+    })
+  }
+})
+
+document.addEventListener('turbolinks:before-cache', function() {
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach( el => {
+      el.removeEventListener('click', () => {
+        const target = el.dataset.target
+        const $target = document.getElementById(target)
+
+        el.classList.toggle('is-active')
+        $target.classList.toggle('is-active')
+      })
+    })
+  }
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,6 +1,5 @@
 Unsplash.configure do |config|
-  config.application_access_key = 'YOUR ACCESS KEY'
-  config.application_secret = 'YOUR APPLICATION SECRET'
-  config.application_redirect_uri = 'https://your-application.com/oauth/callback'
+  config.application_access_key = Rails.application.credentials.unsplash[:access_key]
+  config.application_secret = Rails.application.credentials.unsplash[:secret_key]
   config.utm_source = 'ideaware_home_test_app'
 end
